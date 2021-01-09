a5930f4e2942dfb591fe578a1284936613fcc83a
eraqcode <erikqtrs@gmail.com>
2021-01-09 11:58:12 -0500

    Autenticacion con react y auth0

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,28 @@
+import { useAuth0 } from '@auth0/auth0-react'
 
+import { Login } from './components/auth/Login'
+import { Logout } from './components/auth/Logout'
+import { Profile } from './components/ui/Profile'
 
 function App() {
+
+  const { isAuthenticated } = useAuth0();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+      <div className="button">
+        {
+          !isAuthenticated
+          ?
+            ( <Login /> )
+          :
+            ( <Logout /> )  
+        }
+      </div>
+     
+
+      <Profile />
+    </div>                                                                                                                                                                                                                                                                                     
   );
 }
 
